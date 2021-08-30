e2c1d05f20de8ffd4128f133f31a66ba9e1e891d
Antonio Costa <juninhomathoni99@gmail.com>
2021-08-30 16:33:39 -0300

    leftmost label 6 try

--- src/utils/generatePrintCode.js
+++ src/utils/generatePrintCode.js
@@ -3,15 +3,15 @@ export function generatePrintCode(data) {
     return `
       ^XA
   
-      ^FO125,25^GB570,255,8^FS
-      ^FO140,55^A0,55^FD ${item.PRODUTO}^FS
-      ^FO550,55^A0,25^FD PC ${item.PEDIDO}^FS
-      ^FO550,85^A0,25^FD ${new Intl.DateTimeFormat('pt-BR').format(
+      ^FO20,25^GB570,255,8^FS
+      ^FO35,55^A0,55^FD ${item.PRODUTO}^FS
+      ^FO445,55^A0,25^FD PC ${item.PEDIDO}^FS
+      ^FO445,85^A0,25^FD ${new Intl.DateTimeFormat('pt-BR').format(
         Date.now(),
       )}^FS
-      ^FO140,145^A0,35^FD ${item.DESCRICAO.slice(0, 29)}^FS
-      ^FO140,185^A0,35^FD ${item.DESCRICAO.slice(30, 59)}^FS
-      ^FO140,225^A0,35^FD ${item.DESCRICAO.slice(60, 89)}^FS
+      ^FO35,145^A0,35^FD ${item.DESCRICAO.slice(0, 29)}^FS
+      ^FO35,185^A0,35^FD ${item.DESCRICAO.slice(30, 59)}^FS
+      ^FO35,225^A0,35^FD ${item.DESCRICAO.slice(60, 89)}^FS
   
       ^XZ
       `.repeat(item.SALDO);
