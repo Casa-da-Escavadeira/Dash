623c63448db1b63bd0df70533ec6827a3748d3f8
Antonio Costa <juninhomathoni99@gmail.com>
2021-08-30 15:48:19 -0300

    leftmost label 4 try

--- src/utils/generateSimplePrintCode.js
+++ src/utils/generateSimplePrintCode.js
@@ -6,8 +6,8 @@ export function generateSimplePrintCode(data) {
     ^FO35,25^GB570,255,8^FS
     ^FO50,55^A0,55^FD ${item.PRODUTO}^FS
     )}^FS
-    ^FO50,145^A0,35^FD ${item.DESCRICAO.slice(0, 29)}^FS
-    ^FO50,185^A0,35^FD ${item.DESCRICAO.slice(30, 59)}^FS
+    ^FO50,145^A0,35^FD ${item.DESCRICAO.slice(0, 30)}^FS
+    ^FO50,185^A0,35^FD ${item.DESCRICAO.slice(30, 60)}^FS
     ^FO50,225^A0,35^FD ${item.DESCRICAO.slice(60, 89)}^FS
 
     ^XZ
