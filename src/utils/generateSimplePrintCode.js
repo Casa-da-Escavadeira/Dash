c8bc6c2d9305fbae33e1797a46a653c32b615543
Antonio Costa <juninhomathoni99@gmail.com>
2021-08-30 12:31:07 -0300

    leftmost label

--- src/utils/generateSimplePrintCode.js
+++ src/utils/generateSimplePrintCode.js
@@ -2,14 +2,14 @@ export function generateSimplePrintCode(data) {
   const printItens = data.map(item => {
     return `
       ^XA
-  
-      ^FO125,25^GB570,255,8^FS
-      ^FO140,55^A0,55^FD ${item.PRODUTO}^FS
+    
+      ^FO75,25^GB570,255,8^FS
+      ^FO90,55^A0,55^FD ${item.PRODUTO}^FS
       )}^FS
-      ^FO140,145^A0,35^FD ${item.DESCRICAO.slice(0, 29)}^FS
-      ^FO140,185^A0,35^FD ${item.DESCRICAO.slice(30, 59)}^FS
-      ^FO140,225^A0,35^FD ${item.DESCRICAO.slice(60, 89)}^FS
-  
+      ^FO90,145^A0,35^FD ${item.DESCRICAO.slice(0, 29)}^FS
+      ^FO90,185^A0,35^FD ${item.DESCRICAO.slice(30, 59)}^FS
+      ^FO90,225^A0,35^FD ${item.DESCRICAO.slice(60, 89)}^FS
+
       ^XZ
       `.repeat(item.SALDO);
   });
